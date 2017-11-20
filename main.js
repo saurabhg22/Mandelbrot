9923dd8e2ba1e143171da8fca11d569cb0faf4c9
Saurabh Gupta <saurabhg22596@gmail.com>
2017-11-20 21:54:28 +0530

    Spelling Correction.

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function setup(){
     buttonReset.mousePressed(reset);
 
 
-    buttonShare = createButton('Copy Sharable Link for cuurent view');
+    buttonShare = createButton('Copy Sharable Link for current view');
     buttonShare.position(width + 30, 270);
     buttonShare.mousePressed(share);
     
